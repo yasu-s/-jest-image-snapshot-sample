64abc19d091c58a577ed558d3088e6aa76b4f37f
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-24 19:11:58 +0900

    テスト修正

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 describe('image test', () => {
-  it('ok', () => {
+  it('test1.png', () => {
     const imagePath = path.join(process.cwd(), 'test/test1.png');
     const file = fs.readFileSync(imagePath);
     expect(file).toMatchImageSnapshot();
