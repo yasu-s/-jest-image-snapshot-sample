c129b0229c022df8a152c1ef9af7cddeff4a16f5
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-25 16:40:31 +0900

    ファイル修正

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -2,14 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 describe('image test', () => {
-  it('test1.png ok', () => {
-    const imagePath = path.join(process.cwd(), 'test/test1.png');
-    const file = fs.readFileSync(imagePath);
-    expect(file).toMatchImageSnapshot();
-  });
-
-  it('test2.png ng', () => {
-    const imagePath = path.join(process.cwd(), 'test/test2.png');
+  it('test.png', () => {
+    const imagePath = path.join(process.cwd(), 'test/test.png');
     const file = fs.readFileSync(imagePath);
     expect(file).toMatchImageSnapshot();
   });
