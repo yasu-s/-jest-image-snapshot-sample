618fde53af2e6b616d2a3049010e2646e9984837
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-25 16:14:28 +0900

    不要な設定削除

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   testMatch: ['<rootDir>/test/**/*.+(ts|js)'],
-  resetMocks: true,
   setupFilesAfterEnv: ['./test/jest-setup.ts'],
   modulePathIgnorePatterns: ['jest-setup.ts'],
 };
